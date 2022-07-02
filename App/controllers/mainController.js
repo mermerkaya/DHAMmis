253b0e1c972d90c28b33f53ff3340e5eb20af575
mermerkaya <mermerkaya@gmail.com>
2022-07-02 15:01:19 +0300

    Initial commit

---
+++ App/controllers/mainController.js
@@ -0,0 +1,41 @@
+const fetchManager = require('../utils/fetchManager');
+const appSettings = require('../appSettings');
+
+exports.getHomePage = (req, res, next) => {
+    res.render('home', { isAuthenticated: req.session.isAuthenticated, username: req.session.account?.idTokenClaims?.preferred_username });
+}
+
+exports.getIdPage = (req, res, next) => {
+    const claims = {
+        name: req.session.account.idTokenClaims.name,
+        preferred_username: req.session.account.idTokenClaims.preferred_username,
+        oid: req.session.account.idTokenClaims.oid,
+        sub: req.session.account.idTokenClaims.sub
+    };
+
+    res.render('id', { isAuthenticated: req.session.isAuthenticated, claims: claims });
+}
+
+exports.getProfilePage = async(req, res, next) => {
+    let profile;
+
+    try {
+        profile = await fetchManager.callAPI(appSettings.protectedResources.graphAPI.endpoint, req.session.protectedResources["graphAPI"].accessToken);
+        res.render('profile', { isAuthenticated: req.session.isAuthenticated, profile: profile });
+    } catch (error) {
+        console.log(error);
+        next(error);
+    }
+}
+
+exports.getTenantPage = async(req, res, next) => {
+    let tenant;
+
+    try {
+        tenant = await fetchManager.callAPI(appSettings.protectedResources.armAPI.endpoint, req.session.protectedResources["armAPI"].accessToken);
+        res.render('tenant', { isAuthenticated: req.session.isAuthenticated, tenant: tenant.value[0] });
+    } catch (error) {
+        console.log(error);
+        next(error);
+    }
+}
---
+++ App/routes/router.js
@@ -0,0 +1,56 @@
+const express = require('express');
+const mainController = require('../controllers/mainController');
+
+module.exports = (msid) => {
+
+    // initialize router
+    const router = express.Router();
+
+    // app routes
+    router.get('/', (req, res, next) => res.redirect('/home'));
+    router.get('/home', mainController.getHomePage);
+
+    // auth routes
+    router.get('/signin',
+        msid.signIn({
+            postLoginRedirect: "/",
+            failureRedirect: "/signin"
+        }),
+    );
+
+    router.get('/signout',
+        msid.signOut({
+            postLogoutRedirect: "/",
+        }),
+    );
+
+    // secure routes
+    router.get('/id',
+        msid.isAuthenticated(),
+        mainController.getIdPage
+    );
+
+    router.get('/profile',
+        msid.isAuthenticated(),
+        msid.getToken({
+            resource: msid.appSettings.protectedResources.graphAPI
+        }),
+        mainController.getProfilePage
+    ); // get token for this route to call web API
+
+    router.get('/tenant',
+        msid.isAuthenticated(),
+        msid.getToken({
+            resource: msid.appSettings.protectedResources.armAPI
+        }),
+        mainController.getTenantPage
+    ); // get token for this route to call web API
+
+    // unauthorized
+    router.get('/unauthorized', (req, res) => res.redirect('/401.html'));
+
+    // 404
+    router.get('*', (req, res) => res.redirect('/404.html'));
+
+    return router;
+}
